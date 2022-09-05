58b2b8c537a8314ca1420580a2d255ef433299f7
Jadugar <Grzesiekmorawiec@wp.pl>
2022-09-05 21:42:55 +0200

    Add SignalR to Front and Server Side Chat

---
+++ ServerSide/ServerSide/SignalR_Hub/ChatHubs.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using ServerSide.Models;
+
+public class ChatHub : Hub<IChatClient>
+{
+    public async Task SendMessage(ChatMessage message)
+    {
+        await Clients.All.ReceiveMessage(message);
+    }
+}
+
